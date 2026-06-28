ed1805704669fcdaadb4b5287319839f8c70cf6e
Максим Полозков <pipl82308@gmail.com>
2026-06-28 17:03:05 +0700

    delete files

--- src/redux/actions/authActions.js
+++
@@ -1,8 +0,0 @@
-export const LOGIN_REQUEST = 'LOGIN_REQUEST';
-export const LOGIN_SUCCESS = 'LOGIN_SUCCESS';
-export const LOGIN_ERROR = 'LOGIN_ERROR';
-
-export const loginRequest = (data) => ({
-  type: LOGIN_REQUEST,
-  payload: data
-});
